35145fe002a896ee77c60b36795397e71270d01b
Lawrence <locaulker@gmail.com>
2018-07-12 03:32:24 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <nav-header></nav-header>
+        <card-front></card-front>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue'
+  import CardFront from './components/card/CardFront.vue'
+
+  export default {
+    components: {
+      navHeader: Header,
+      cardFront: CardFront
+    }
+  }
+</script>
+
+<style>
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    color: #333;
+  }
+
+  a {
+    cursor: pointer;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 class="text-center">Creative Cards</h1>
+    <ul class="nav justify-content-center">
+      <li class="nav-item"> <a href="" class="nav-link">Front</a></li>
+      <li class="nav-item"> <a href="" class="nav-link">Inside Left</a></li>
+      <li class="nav-item"> <a href="" class="nav-link">Inside Right</a></li>
+      <li class="nav-item"> <a href="" class="nav-link">Back</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+  h1 {
+    margin-top: 10px;
+  }
+</style>
+
+
